21708a95c0f65c8fb4b17c23b2b565099d169822
ob-taka <xiongrunlinxiong@gmail.com>
2020-04-24 14:10:51 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,53 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
 public class Question5
 {
+  
+  public static int[] addX(int n, int arr[], int x) 
+    { 
+        int i; 
+        int newarr[] = new int[n + 1]; 
+        for (i = 0; i < n; i++) 
+            newarr[i] = arr[i]; 
+  
+        newarr[n] = x; 
+  
+        return newarr; 
+    } 
+  
+   public static int mode(int[] input) {
+
+    int[] count = new int[101];
+
+    for (int i=0; i < input.length; i++) {
+        count[input[i]]++;
+    }
+     
+    int index = count.length-1;
+    for (int i=count.length-2; i >=0; i--) {
+        if (count[i] >= count[index])
+            index = i;
+    }
+
+    return index;
+}
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
+     Scanner _input = new Scanner(System.in);
     
+     System.out.print("Enter the amount of number to be keyed in: ");
+     int count = _input.nextInt();
+    
+     int arry[] = new int[] {};
+
+     for (int i = count; i > 0; i--){
+       System.out.print("Enter one number at a time: ");
+       int digit = _input.nextInt();
+       arry = addX(arry.length, arry, digit);
+     }
+
+     int result = mode(arry);
+     System.out.print("The mode is " + result);
+
   }
 }
